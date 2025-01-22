852cbc231d5f9d87b6911fe77d7b6e53e2f377a9
Leticia Peña <leticia.penhag@fpuna.edu.py>
2025-01-22 15:45:38 -0300

    code cleaning

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,21 +1,3 @@
-/*const User = require('../models/user');
-
-module.exports = {
-    async getAll(req, res, next) {
-        try {
-            const data = await User.getAll();
-            console.log(`Usuarios: ${data}`);
-            return res.status(201).json(data);
-        } catch (error) {
-            console.log(`Error: ${error}`);
-            return res.status(501).json({
-                success:false,
-                message: `Error al obtener los usuarios`
-            });
-        }
-    }
-};*/
-
 const User = require('../models/user');
 
 const UsersController = {};
