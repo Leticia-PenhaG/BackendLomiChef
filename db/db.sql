76ae9b3c8b290d962c8aeb797d11df1b95eddab6
Leticia Peña <leticia.penhag@fpuna.edu.py>
2025-01-25 23:53:14 -0300

    se actualiza la bd con usuarios, roles y la tabla intermedia, se crea el modelo de roles

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,18 @@
+-- Tabla: roles
+DROP TABLE IF EXISTS roles CASCADE;
+
+CREATE TABLE roles (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(180) NOT NULL,
+    image VARCHAR(255),
+    route VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Tabla: users
+DROP TABLE IF EXISTS users CASCADE;
+
 CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
     id_customer VARCHAR(255),
@@ -11,4 +26,60 @@ CREATE TABLE users (
     session_token VARCHAR(180),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Tabla intermedia: user_has_roles
+DROP TABLE IF EXISTS user_has_roles CASCADE;
+
+CREATE TABLE user_has_roles (
+    id_user BIGINT NOT NULL,
+    id_role BIGINT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id_user, id_role),
+    FOREIGN KEY (id_user) REFERENCES users (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (id_role) REFERENCES roles (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+INSERT INTO roles (
+    name, 
+    route,
+    created_at,
+    updated_at
+)
+
+VALUES(
+    'CLIENTE',
+    'client/product/list',
+    '2025-01-25',
+    '2025-01-25'
+);
+
+INSERT INTO roles (
+    name, 
+    route,
+    created_at,
+    updated_at
+)
+
+VALUES(
+    'RESTAURANTE',
+    'restaurant/orders/list',
+    '2025-01-25',
+    '2025-01-25'
+);
+
+INSERT INTO roles (
+    name, 
+    route,
+    created_at,
+    updated_at
+)
+
+VALUES(
+    'REPARTIDOR',
+    'delivery/orders/list',
+    '2025-01-25',
+    '2025-01-25'
 );
